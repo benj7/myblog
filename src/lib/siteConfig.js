47e2d4150b28eab7e785b3101dec605b002ca127
Benjamin DAVID <benjamin7david@gmail.com>
2022-08-27 11:45:16 +0200

    Update siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -8,7 +8,9 @@ export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = '7benj7';
 export const MY_YOUTUBE = '';
+export const process.env.GH_TOKEN = 5000;
 export const POST_CATEGORIES = ['Blog'] // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
+
 
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
