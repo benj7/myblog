26cc2bbf889dc453215bdfcd49402cbf61e28e7d
Benjamin DAVID <benjamin7david@gmail.com>
2022-08-27 11:37:34 +0200

    Update siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,13 +1,13 @@
-export const SITE_URL = 'https://swyxkit.netlify.app';
-export const APPROVED_POSTERS_GH_USERNAME = ['sw-yx'];
-export const GH_USER_REPO = 'sw-yx/swyxkit'; // used for pulling github issues and offering comments
+export const SITE_URL = 'https://benjamindavid.netlify.app';
+export const APPROVED_POSTERS_GH_USERNAME = ['benj7'];
+export const GH_USER_REPO = 'benj7/swyxkit'; // used for pulling github issues and offering comments
 export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
-export const SITE_TITLE = 'SwyxKit';
+export const SITE_TITLE = 'My personal blog';
 export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
-export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
+export const MY_TWITTER_HANDLE = '7benj7';
+export const MY_YOUTUBE = '';
 export const POST_CATEGORIES = ['Blog'] // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 
 // dont forget process.env.GH_TOKEN
