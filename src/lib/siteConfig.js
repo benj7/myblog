dec45ed88e40adb5e22cf06b8c39a07b967d58cc
Benjamin DAVID <benjamin7david@gmail.com>
2022-08-28 15:13:44 +0200

    Update siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -8,7 +8,6 @@ export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = '7cpt7';
 export const MY_YOUTUBE = '';
-process.env.GH_TOKEN;
 export const POST_CATEGORIES = ['Blog'] // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 
 
