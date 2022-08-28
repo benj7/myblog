5c49842460fede3a93ed988b4d60ae4ced406e50
Benjamin DAVID <benjamin7david@gmail.com>
2022-08-28 14:34:53 +0200

    Update siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -3,12 +3,12 @@ export const APPROVED_POSTERS_GH_USERNAME = ['benj7'];
 export const GH_USER_REPO = 'benj7/myblog'; // used for pulling github issues and offering comments
 export const REPO_URL = 'https://github.com/benj7';
 export const SITE_TITLE = 'Benjamin DAVID';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const SITE_DESCRIPTION = "R and Shiny, Economics and Investmen strategies Stuff";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = '7cpt7';
 export const MY_YOUTUBE = '';
-//export const process.env.GH_TOKEN = 5000;
+export const process.env.GH_TOKEN = 5000;
 export const POST_CATEGORIES = ['Blog'] // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 
 
