ed886cc6fe7f4cd8f0b35552115b5a1c67df2fa0
Benjamin DAVID <benjamin7david@gmail.com>
2022-08-28 12:18:19 +0200

    Update siteConfig.js

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,7 +1,7 @@
 export const SITE_URL = 'https://benjamindavid.netlify.app';
 export const APPROVED_POSTERS_GH_USERNAME = ['benj7'];
 export const GH_USER_REPO = 'benj7/myblog'; // used for pulling github issues and offering comments
-export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const REPO_URL = 'https://github.com/benj7';
 export const SITE_TITLE = 'My personal blog';
 export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
 export const DEFAULT_OG_IMAGE =
